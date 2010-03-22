ed8a9acfc741c93af3030822146c08d0131f3e04
Florian Schulze <florian.schulze@gmx.net>
2010-03-22 15:38:02 +0100

    Fix pmate.

--- aliases.zsh
+++ aliases.zsh
@@ -9,7 +9,11 @@ alias sha1="openssl sha1"
 alias d="develop"
 alias g="git"
 alias gs="git svn"
-alias pmate="mate *.cfg CHANGES* bin/ cfgs/ etc/ src/ templates/ production/ $*"
 alias jslint="java org.mozilla.javascript.tools.shell.Main /Users/fschulze/Development/jslint.js"
 alias srcgrep="grep --exclude \"*.pyc\" --exclude \"*.svn-base\" --exclude \"all-wcprops\" --exclude \"entries\" --exclude \"*.tmp\" -r"
 alias srcdiff="diff -ru -x .svn -x .git -x *.py[co] -x *.egg-info"
+
+function pmate () {
+    setopt local_options no_nomatch
+    mate *.cfg CHANGES* bin/ cfgs/ etc/ src/ templates/ production/ $*
+}
