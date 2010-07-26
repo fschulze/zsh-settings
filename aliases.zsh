0f0aabdc209e0454292ad10dfb8d1a65f5659b65
Florian Schulze <florian.schulze@gmx.net>
2010-07-26 11:07:30 +0200

    Add "m" and "pm" aliases for textmate. Add experimental dmate. Add "src" to pmate.

--- aliases.zsh
+++ aliases.zsh
@@ -8,6 +8,7 @@ alias vi="vim"
 alias sha1="openssl sha1"
 alias d="develop"
 alias g="git"
+alias m="mate"
 alias gs="git svn"
 alias jslint="java org.mozilla.javascript.tools.shell.Main /Users/fschulze/Development/jslint.js"
 alias srcgrep="grep --exclude \"*.pyc\" --exclude \"*.svn-base\" --exclude \"all-wcprops\" --exclude \"entries\" --exclude \"*.tmp\" -r"
@@ -15,7 +16,18 @@ alias srcdiff="diff -ru -x .svn -x .git -x *.py[co] -x *.egg-info"
 
 function pmate () {
     setopt local_options no_nomatch
-    mate *.cfg CHANGES* bin/ cfgs/ docs/ etc/ templates/ production/ $*
+    mate *.cfg CHANGES* bin/ cfgs/ docs/ etc/ src/ templates/ production/ $*
+}
+alias pm="pmate"
+
+function dmate () {
+    setopt local_options no_nomatch
+    if /Users/fschulze/bin/develop info -a -p > /dev/null; then
+        sources=$(/Users/fschulze/bin/develop info -a -p)
+    fi
+    sources=${${${(f)sources}#$(pwd)/}/%/\/}
+    echo mate *.cfg CHANGES* bin/ cfgs/ docs/ etc/ ${=sources} templates/ production/ $*
+    mate *.cfg CHANGES* bin/ cfgs/ docs/ etc/ ${=sources} templates/ production/ $*
 }
 
 source ~/.zsh/jump.zsh
