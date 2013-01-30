6145ea956b9cedb3bf6fde926dc8f5a010c3311b
Florian Schulze <florian.schulze@gmx.net>
2013-01-30 18:16:56 +0100

    Variables for locally installed Perl libs.

--- profile.zsh
+++ profile.zsh
@@ -29,4 +29,10 @@ export PYTHONSTARTUP=$HOME/.pythonrc.py
 
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
+export PERL_LOCAL_LIB_ROOT="$HOME/perl5";
+export PERL_MB_OPT="--install_base $HOME/perl5";
+export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5";
+export PERL5LIB="$HOME/perl5/lib/perl5/darwin-thread-multi-2level:$HOME/perl5/lib/perl5";
+export PATH="$HOME/perl5/bin:$PATH";
+
 export PGHOST=localhost
